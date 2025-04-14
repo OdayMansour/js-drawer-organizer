502eac66825b86f6f0652cf7ba5f91b46be2e519
Oday Mansour <odaym@nsour.net>
2025-04-14 22:36:51 +0100

    Scale drawing to fill most of the page

--- src/main.js
+++ src/main.js
@@ -9,19 +9,33 @@ document.oncontextmenu = document.body.oncontextmenu = function() {return false;
 // Initialize Paper.js
 paper.install(window);
 window.onload = async function() {
-    // Setup Paper.js
-    paper.setup('canvas');
-
-    // Get canvas dimensions from user
     const canvasSetup = new CanvasSetup();
     const dimensions = await canvasSetup.promptForDimensions();
 
-    // Canvas dimensions
-    var canvasWidth = dimensions.width;
-    var canvasHeight = dimensions.height;
+    // Drawing dimensions
+    var drawingWidth = dimensions.width;
+    var drawingHeight = dimensions.height;
+
+    // Setup Paper.js
+    paper.setup('canvas');
+
+    // Get size of drawing, size of canvas, and derive scaling factor needed to fill 90% of screen
+    const scaleFactor = Math.min(
+        window.innerWidth / drawingWidth,
+        window.innerHeight / drawingHeight
+    ) * 0.9;
+
+    // Move the center of the drawing to prepare for scaling
+    paper.view.center = new Point(
+        paper.view.center.x / scaleFactor * 0.9,
+        paper.view.center.y / scaleFactor * 0.9
+    );
+
+    // Scale the drawing (scaling happens relative to center of drawing, not top left {0,0})
+    paper.view.scaling = scaleFactor;
 
     // Create a new BSP tree
-    const bspTree = new BSPTree(canvasWidth, canvasHeight);
+    const bspTree = new BSPTree(drawingWidth, drawingHeight);
 
     // Initialize interaction handlers
     const interactionHandler = new InteractionHandler(bspTree);
@@ -41,7 +55,7 @@ window.onload = async function() {
     });
 
     document.getElementById('reset').addEventListener('click', function() {
-        bspTree.root = new BSPNode(new Rectangle(0, 0, canvasWidth, canvasHeight, 1));
+        bspTree.root = new BSPNode(new Rectangle(0, 0, drawingWidth, drawingHeight, 1));
         bspTree.nextId = 2;
         interactionHandler.selectedNode = null;
         renderer.renderBSPTree(bspTree);
